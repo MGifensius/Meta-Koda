96c751a055b3c8478b92fc6022562060b398f811
Marchelino Gifensius ( Business Analyst - IT - Commercial Poultry - Commercial Poultry Indonesia ) <10105960@japfacomfeed.co.id>
2026-04-28 22:13:40 +0700

    feat(config): add shared @buranchi/tsconfig and @buranchi/eslint-config packages

---
+++ packages/config/eslint-config/base.cjs
@@ -0,0 +1,16 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    'import/order': ['warn', { 'newlines-between': 'always', alphabetize: { order: 'asc' } }],
+  },
+};
